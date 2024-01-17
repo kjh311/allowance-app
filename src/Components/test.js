0b2feaf482b452e87c78e1a8c851c0d9dc80ba0a
kjh311 <kjh311@gmail.com>
2024-01-17 14:47:00 -0700

    working again

---
+++ src/Components/test.js
@@ -0,0 +1,13 @@
+// import "./App.css";
+
+function Test() {
+  return (
+    <div className="Test">
+      <header className="Test-header">
+        <h1>Test!!!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Test;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom"; // Import react-dom as a default import
+
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -19,7 +20,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
