e570c44d0405d85d26ee9777c58877fd7af0c55a
kjh311 <kjh311@gmail.com>
2024-01-30 10:45:47 -0700

    adding color works

--- src/Components/ColorCrud.js
+++ src/Components/ColorCrud.js
@@ -1,9 +1,17 @@
 import { db } from "../firebase";
 import { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
+// import { Form, Button } from "react-bootstrap";
 
 function ColorCrud() {
+  //set state for loading colors and values in database
   const [colors, setColors] = useState([]);
+
+  //set state for adding new colors and values
+  const [newColor, setNewColor] = useState("");
+  const [newValue, setNewValue] = useState("");
+
+  //variable that points to database collection
   const colorsCollectionRef = collection(db, "color");
 
   //useEffect loads data when page loads
@@ -11,19 +19,46 @@ function ColorCrud() {
     const getColors = async () => {
       const data = await getDocs(colorsCollectionRef);
       setColors(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+
+      //temp to make sure data is loading correctly
+      console.log(data);
     };
 
     getColors();
   }, []);
 
+  //create color in database. Send value from input to useState variables to this function:
+  const createColor = async () => {
+    await addDoc(colorsCollectionRef, { name: newColor, value: newValue });
+  };
+
   return (
     <div className="color-service-div">
-      {colors.map((color, index) => {
+      <input
+        placeholder="Color..."
+        onChange={(event) => {
+          setNewColor(event.target.value);
+        }}
+      />
+
+      <input
+        placeholder="Value..."
+        onChange={(event) => {
+          setNewValue(event.target.value);
+        }}
+      />
+
+      <button onClick={createColor} className="add-button">
+        Add Color
+      </button>
+
+      {colors.map((color, id) => {
         // Use a unique identifier for the key, for example, the index or color.id
         return (
-          <div key={index}>
-            <h1>Color: {color.name}</h1>
-            <h1>Value: {color.value}</h1>
+          <div key={color.id}>
+            <h1 style={{ backgroundColor: color.value }}>
+              Color: {color.name}
+            </h1>
           </div>
         );
       })}
