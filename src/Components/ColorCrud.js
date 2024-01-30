7f6f774952b2d1c5462beb1d222532271b4f0a41
kjh311 <kjh311@gmail.com>
2024-01-30 14:05:37 -0700

    update works

--- src/Components/ColorCrud.js
+++ src/Components/ColorCrud.js
@@ -1,7 +1,12 @@
 import { db } from "../firebase";
 import { useState, useEffect } from "react";
-import { collection, getDocs, addDoc } from "firebase/firestore";
-// import { Form, Button } from "react-bootstrap";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
 
 function ColorCrud() {
   //set state for loading colors and values in database
@@ -10,6 +15,10 @@ function ColorCrud() {
   //set state for adding new colors and values
   const [newColor, setNewColor] = useState("");
   const [newValue, setNewValue] = useState("");
+
+  //set state for updating colors and values
+  const [updatedColor, updateColorFromInput] = useState("");
+  const [updatedValue, updateValueFromInput] = useState("");
 
   //variable that points to database collection
   const colorsCollectionRef = collection(db, "color");
@@ -32,33 +41,65 @@ function ColorCrud() {
     await addDoc(colorsCollectionRef, { name: newColor, value: newValue });
   };
 
+  //update color and value
+  const updateColor = async (id) => {
+    const colorDoc = doc(db, "color", id);
+    console.log(updatedColor, updatedValue);
+    const newFields = { name: updatedColor, value: updatedValue };
+    await updateDoc(colorDoc, newFields);
+  };
+
   return (
     <div className="color-service-div">
-      <input
-        placeholder="Color..."
-        onChange={(event) => {
-          setNewColor(event.target.value);
-        }}
-      />
+      <div className="add-color-form">
+        <label htmlFor="colorInput">Add New Color:</label>
+        <br />
+        <input
+          id="colorInput"
+          placeholder="Color..."
+          onChange={(event) => {
+            setNewColor(event.target.value);
+          }}
+        />
 
-      <input
-        placeholder="Value..."
-        onChange={(event) => {
-          setNewValue(event.target.value);
-        }}
-      />
+        <br />
 
-      <button onClick={createColor} className="add-button">
-        Add Color
-      </button>
+        <input
+          placeholder="Value..."
+          onChange={(event) => {
+            setNewValue(event.target.value);
+          }}
+        />
+        <br />
+
+        <button onClick={createColor} className="add-button">
+          Add Color
+        </button>
+      </div>
 
       {colors.map((color, id) => {
         // Use a unique identifier for the key, for example, the index or color.id
         return (
           <div key={color.id}>
             <h1 style={{ backgroundColor: color.value }}>
-              Color: {color.name}
+              {color.name} {color.value}
             </h1>
+
+            <input
+              placeholder="Update Color Name..."
+              onChange={(event) => {
+                updateColorFromInput(event.target.value);
+              }}
+            />
+
+            <input
+              placeholder="Update Value..."
+              onChange={(event) => {
+                updateValueFromInput(event.target.value);
+              }}
+            />
+
+            <button onClick={() => updateColor(color.id)}>Edit:</button>
           </div>
         );
       })}
